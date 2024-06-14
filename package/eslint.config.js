5ffe5cf2671af3663f6c0bede4f413dcb6c33289
zoebasil <dog456pop@gmail.com>
2024-06-13 20:38:05 -0600

    add plugins, change package, fix eslint config

--- package/eslint.config.js
+++ package/eslint.config.js
@@ -1,9 +1,35 @@
 // @ts-check
-
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginNoAutofix from 'eslint-plugin-no-autofix';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
+	eslintConfigPrettier,
+	eslintPluginNoAutofix,
+	{
+		rules: {
+			'prettier/prettier': 'warn',
+			'@typescript-eslint/no-floating-promises': [
+				'error',
+				{ ignoreVoid: true },
+			],
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_',
+				},
+			],
+		},
+		plugins: {
+			'prettier': eslintPluginPrettier,
+			'simple-import-sort': eslintPluginSimpleImportSort,
+		}
+	}
 );
